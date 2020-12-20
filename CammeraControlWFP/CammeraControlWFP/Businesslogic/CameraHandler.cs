0293592da0a2afa69e2b12d507a61c943b38a471
Aleksey <akimov228aleksei@mail.ru>
2020-12-20 22:54:14 +0300

    add laba 2

---
+++ CammeraControlWFP/CammeraControlWFP/Businesslogic/CameraHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace CammeraControlWFP.Businesslogic
+{
+    public class CameraHandler
+    {
+        private static TcpClient client;
+        private Action<string> _callback;
+        private static NetworkStream clientStream;
+
+        public CameraHandler( Action<string> callback)
+        {
+            _callback = callback;
+        }
+
+        public void startPoling()
+        {
+            Connect();
+            clientStream = client.GetStream();
+            Int32 bytes;
+            String responseData = String.Empty;
+            Byte[] data = new Byte[256];
+
+            while (true)
+            {
+                bytes = clientStream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+
+                // Read the second batch of the TcpServer response bytes. 
+                bytes = clientStream.Read(data, 0, data.Length);
+                responseData = responseData + System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+
+                //response data sampel: 2 codes with content "ABC-abc-1234"
+                //1234L000000043{0D}{0A}1234star;1;ABC-abc-1234;ABC-abc-1234;stop
+                //ticket + length + /r/n + ticket + data + /r/n
+                //extract data from receive string
+                //extract length information
+                int lengthData = int.Parse(responseData.Substring(5, 9));
+                //extract data from string
+                string stringData = responseData.Substring(20, (lengthData - 6));
+                if (stringData.Equals("star;0;;stop"))
+                {
+                    _callback("No data available");
+                }
+                else
+                {
+                    _callback(stringData);
+                }
+
+            }
+        }
+        private static void Connect()
+        {
+            //Open connection to O2I
+            client = new TcpClient();
+            //connection data O2I50x
+            client.Connect("10.151.1.130", 50010);
+        }
+    }
+}
---
+++ CammeraControlWFP/CammeraControlWFP/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿using CammeraControlWFP.Businesslogic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CammeraControlWFP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private CameraHandler _cameraHandler;
+        private BindingList<string> _records = new BindingList<string>();
+        private string _precentage = "No data";
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void updateData(string data)
+        {
+            _records.Add(data);
+            recalculatePrecentage();
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            _records.ListChanged += new System.ComponentModel.ListChangedEventHandler(_records_ListChanged);
+            _cameraHandler = new CameraHandler(updateData);
+            Thread thread = new Thread(new ThreadStart(_cameraHandler.startPoling));
+            thread.Start();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(274, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clean up data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 338);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Percentage of successfully recognized:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(226, 335);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(60, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(274, 316);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 361);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Records recived";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(226, 358);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(60, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 424);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "CameraControl";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+         
+        private void _records_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            recalculatePrecentage();;
+        }
+
+        private void recalculatePrecentage()
+        {
+            if (_records.Count == 0)
+            {
+                this.textBox1.Text = "No data";
+            }
+            else
+            {
+                double precentage = (double)(_records.Where(n => !n.Equals("No data available")).Count()) /
+                    (double)_records.Count
+                    * 100;
+                _precentage = $"{precentage.ToString("F")} %";
+            }
+
+            listBox1.Invoke(new Action(() => listBox1.Items.Clear()));
+            foreach (var a in _records)
+            {
+                listBox1.Invoke(new Action(() => listBox1.Items.Add(a)));
+            }
+
+            textBox1.Invoke(new Action(() => textBox1.Text = _precentage));
+            textBox2.Invoke(new Action(() => textBox2.Text = _records.Count().ToString()));
+        }
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private Label label2;
+        private TextBox textBox2;
+    }
+}
+
